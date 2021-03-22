1ae74dfc19558ce6ea43eabd4750ce597ebdd4df
Franco3010 <35970825+Franco3010@users.noreply.github.com>
2021-03-22 18:43:26 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -31,7 +31,16 @@ let score = 0;
 let Realscore = "";
 let stop = 0
 let indexPipe
+let indexWire
+let indexBike
+let indexBike2
+let indexGround
+let indexBee
+let indexBee2
+
+
 let drawTime = 0
+
 const init = async () => {
   // const stage = createStage();
   // const canvas = stage.getCanvas();
@@ -51,6 +60,8 @@ const init = async () => {
   const skyImg = await loadTexture(gl, './sky.jpg');
   const groundsImg = await loadTexture(gl, './base.jpg');
   const bee1Img = await loadTexture(gl, './bee01.png');
+  const bee2Img = await loadTexture(gl, './bee02.png');
+
   const pipeImg = await loadTexture(gl, './pipe.png');
   const pipeDownImg = await loadTexture(gl, './pipe-down.png');
   const signImg = await loadTexture(gl, './sign.png');
@@ -85,27 +96,6 @@ const init = async () => {
     x: 5,
     y: 40
   }
-  const gr4 = {
-    x: 40,
-    y: 90
-  }
-  const gr3 = {
-    x: 30,
-    y: 90
-  }
-  const gr2 = {
-    x: 20,
-    y: 90
-  }
-  const gr1 = {
-    x: 10,
-    y: 90
-  }
-  const gr0 = {
-    x: 0,
-    y: 90
-  }
-
 
   const mainAtlas = await loadAtlas(gl, './enemy.atlas', {});
   const runRegions = mainAtlas.findRegions('boss_projectile_A');
@@ -116,8 +106,23 @@ const init = async () => {
 
 
   const bees01: { x: number; y: number; powerup: number }[] = [];
+  const bees02: { x: number; y: number; powerup: number }[] = [];
 
-  const bikes: { x: number; y: number; speedup: number }[] = [];
+  const bikes1: { x: number; y: number; speedup: number }[] = [
+    {
+      x: 25,
+      y: 80,
+      speedup: 0
+    }
+  ];
+  const bikes2: { x: number; y: number; speedup: number }[] = [
+    {
+      x: 15,
+      y: 80,
+      speedup: 0
+    }
+  ];
+
   const pipes: { x: number; y: number; }[] = [];
   const trees: { x: number; y: number; }[] = [
     {
@@ -129,7 +134,16 @@ const init = async () => {
     {
       x: 0,
       y: 65
+    },
+    {
+      x: 25,
+      y: 65
+    },
+    {
+      x: 50,
+      y: 65
     }
+
   ];
   let treeIndex = 0;
   let wireIndex = 0;
@@ -137,8 +151,7 @@ const init = async () => {
   //   x: 40,
   //   y: 80
   // }
-  const grounds: { x: number; y: number }[] = [];
-  const gr: { x: number; y: number }[] = [
+  const grounds: { x: number; y: number }[] = [
     {
       x: 0,
       y: 90
@@ -158,6 +171,10 @@ const init = async () => {
     {
       x: 40,
       y: 90
+    },
+    {
+      x: 50,
+      y: 90
     }
   ];
 
@@ -168,6 +185,7 @@ const init = async () => {
   let accumulate = 0;//?
   let accumulate1 = 0;//?
   let accuTree = 0;
+  let accumulate2 = 0
 
   const mole_SIZE = 10;
 
@@ -199,18 +217,27 @@ const init = async () => {
     }
     accumulate += delta;
     accumulate1 += delta;
+    accumulate2 += delta;
+
     accuTree += delta
 
 
-    if (accumulate1 > 1.5 && stop == 0) {
-      accumulate1 = 0;
+    if (accumulate2 > 3 && stop == 0) {
+      accumulate2 = 0;
 
-      bikes.push({
+      bikes1.push({
         x: 25,
         y: 80,
         speedup: Math.random()
+
       })
 
+      bikes2.push({
+        x: 15,
+        y: 80,
+        speedup: Math.random()
+
+      })
     }
 
 
@@ -220,7 +247,14 @@ const init = async () => {
       bees01.push(
         {
           x: 50,
-          y: 30,
+          y: Math.floor(Math.random() * (50 - 30 + 1)) + 30,
+          powerup: Math.random()
+        }
+      )
+      bees02.push(
+        {
+          x: 70,
+          y: Math.floor(Math.random() * (60 - 20 + 1)) + 20,
           powerup: Math.random()
         }
       )
@@ -234,50 +268,50 @@ const init = async () => {
 
 
     }
-    if (trees[treeIndex].x < -30 && stop == 0) {
+
+    for (let wire of wires) {
+      indexWire = wires.indexOf(wire)
+
+      if (accumulate1 > 4 && stop == 0) {
+        accumulate1 = 0
+
+        wires.push(
+          {
+            x: 50,
+            y: 65,
+          }
+        )
 
 
-      trees.push(
-        {
-          x: 50,
-          y: 80,
-        }
-      )
-      treeIndex = treeIndex + 1
-
-    }
-    if (wires[wireIndex].x < -9 && stop == 0) {
-
-
-      wires.push(
-        {
-          x: 50,
-          y: 65,
-        }
-      )
-      wireIndex = treeIndex + 1
-
+      }
+      if (wire.x < -25) {
+        wires.splice(indexWire, 1)
+      }
     }
 
 
+    for (let ground of grounds) {
+      indexGround = grounds.indexOf(ground)
+
+      if (accuTree > 0.3 && stop == 0) {
+        accuTree = 0
+
+        grounds.push(
+          {
+            x: 49,
+            y: 90,
+          }
+        )
 
 
-    grounds.push({
-      x: 1 * gr[0].x,
-      y: 90
-    })
-    grounds.push({
-      x: 1 * gr[4].x,
-      y: 90
-    })
-    grounds.push({
-      x: 1 * gr[3].x,
-      y: 90
-    })
-    grounds.push({
-      x: 1 * gr[2].x,
-      y: 90
-    })
+      }
+      if (ground.x < -50) {
+        grounds.splice(indexGround, 1)
+      }
+    }
+
+
+
 
 
 
@@ -319,34 +353,26 @@ const init = async () => {
 
     for (let wire of wires) {
 
-      batch.draw(sora, wire.x, wire.y - 70, 20, 40)
-      batch.draw(sora, wire.x + 20, wire.y - 70, 20, 40)
-      batch.draw(sora, wire.x + 40, wire.y - 70, 20, 40)
 
-      batch.draw(skyImg, wire.x, wire.y - 30, 20, 25)
-      batch.draw(skyImg, wire.x + 20, wire.y - 30, 20, 25)
-      batch.draw(skyImg, wire.x + 40, wire.y - 30, 20, 25)
+      batch.draw(skyImg, wire.x, wire.y - 70, 25, 75)
 
-      batch.draw(cloud, wire.x, wire.y - 10, 20, 25)
-      batch.draw(cloud, wire.x + 20, wire.y - 10, 20, 25)
-      batch.draw(cloud, wire.x + 40, wire.y - 10, 20, 25)
 
-      batch.draw(house, wire.x, wire.y, 20, 25)
-      batch.draw(house, wire.x + 20, wire.y, 20, 25)
-      batch.draw(house, wire.x + 40, wire.y, 20, 25)
+      batch.draw(cloud, wire.x, wire.y - 3, 25, 25)
 
-      batch.draw(wireImg, wire.x, wire.y, 20, 25)
-      batch.draw(wireImg, wire.x + 20, wire.y, 20, 25)
-      batch.draw(wireImg, wire.x + 40, wire.y, 20, 25)
+
+      batch.draw(house, wire.x, wire.y, 25, 25)
+
+
+      batch.draw(wireImg, wire.x, wire.y, 25, 25)
+
+      batch.draw(treeImg, wire.x, wire.y + 15, 25, 10)
 
 
 
 
 
 
-      // if (stop == 1) {
-      //   break;
-      // }
+
       if (stop == 0) {
         wire.x = wire.x - 0.1
       }
@@ -357,59 +383,88 @@ const init = async () => {
     }
 
     for (let bee of bees01) {
-      batch.draw(bee1Img, bee.x, bee.y, 10, 10)
-      // if (stop == 1) {
-      //   break;
-      // }
-      if (bee.powerup < 0.3 && stop == 0) {
+      indexBee = bees01.indexOf(bee)
+      batch.draw(bee1Img, bee.x, bee.y, 20, 10)
+
+      if (bee.powerup < 0.4 && stop == 0) {
         bee.x = bee.x - 0.2
       }
       else {
         if (stop == 0) {
-          bee.x = bee.x - 1
+          bee.x = bee.x - 0.5
         }
 
 
       }
+      if (bee.x < -200) {
+        bees01.splice(indexBee, 1)
+      }
     }
-    for (let tree of trees) {
 
-      batch.draw(treeImg, tree.x, tree.y, 40, 10)
-      batch.draw(treeImg, tree.x + 40, tree.y, 40, 10)
+    for (let bee of bees02) {
+      indexBee2 = bees02.indexOf(bee)
+      batch.draw(bee2Img, bee.x, bee.y, 5, 5)
+
+      if (bee.powerup < 0.2 && stop == 0) {
+        bee.x = bee.x - 0.2
+      }
+      else {
+        if (stop == 0) {
+          bee.x = bee.x - 0.8
+        }
+
+
+      }
+      if (bee.x < -350) {
+        bees02.splice(indexBee2, 1)
+      }
+    }
 
 
 
+    for (let bike of bikes1) {
+      indexBike = bikes1.indexOf(bike)
+
+      batch.draw(bikeImg, bike.x, bike.y, 60, 10)
+
+      if (stop == 0) {
+        bike.x = bike.x - 0.3
+
+
+      }
+      if (bike.x < -200) {
+        bikes1.splice(indexBike, 1)
+      }
+
+
+
+
+
+
+
+
+    }
+    for (let bike of bikes2) {
+      indexBike2 = bikes2.indexOf(bike)
+
+      batch.draw(bike2Img, bike.x, bike.y, 60, 10)
 
 
       if (stop == 0) {
-        tree.x = tree.x - 0.1
+        bike.x = bike.x - 0.5
+
+
+      }
+      if (bike.x < -200) {
+        bikes2.splice(indexBike2, 1)
       }
 
 
 
 
-      // if (stop == 1) {
-      //   break;
-      // }
-    }
 
 
-    for (let bike of bikes) {
-      if (bike.speedup < 0.2) {
-        batch.draw(bikeImg, bike.x, bike.y, 50, 10)
 
-        if (stop == 0) {
-          bike.x = bike.x - 0.2
-        }
-      }
-      else {
-        batch.draw(bike2Img, bike.x - 10, bike.y, 50, 10)
-        if (stop == 0) {
-          bike.x = bike.x - 0.4
-        }
-
-
-      }
 
     }
     accumulateScore += delta
@@ -494,7 +549,10 @@ const init = async () => {
 
     for (let ground of grounds) {
       batch.draw(groundsImg, ground.x, ground.y, 10, 10)
-      ground.x = ground.x - 0.5
+      if (stop == 0) {
+        ground.x = ground.x - 0.5
+      }
+
     }
 
 
@@ -502,7 +560,7 @@ const init = async () => {
     if (stop == 0) {
       runAnimation
         .getKeyFrame(stateTime, PlayMode.LOOP)
-        .draw(batch, bird.x, bird.y, 5, 5);
+        .draw(batch, bird.x, bird.y, 4, 4);
     }
     else {
       runAnimation1
