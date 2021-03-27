e7e4801e09836573b45703052e3871b9396d1ee9
Franco3010 <ngodu.anh3010@gmail.com>
2021-03-28 00:33:23 +0700

    up

--- src/index.tsx
+++ src/index.tsx
@@ -36,6 +36,9 @@ let indexWire;
 let indexBike;
 let indexBike2;
 let indexGround;
+let indexHouse;
+let indexTree;
+let indexSky;
 let indexBee;
 let indexBee2;
 let Time_gia_toc = 0;
@@ -45,6 +48,10 @@ let lastIndexGround;
 let lastIndexWire;
 let lastIndexBee01;
 let lastIndexBee02;
+let lastIndexCloud;
+let lastIndexSky;
+let lastIndexTree;
+let lastIndexHouse;
 
 const init = async () => {
   // const stage = createStage();
@@ -70,8 +77,8 @@ const init = async () => {
   const signImg = await loadTexture(gl, './sign.png');
   const treeImg = await loadTexture(gl, './tree.png');
   const wireImg = await loadTexture(gl, './wire.png');
-  const house = await loadTexture(gl, './house.png');
-  const cloud = await loadTexture(gl, './cloud.png');
+  const houseImg = await loadTexture(gl, './house.png');
+  const cloudImg = await loadTexture(gl, './cloud.png');
   const sora = await loadTexture(gl, './sora.jpg');
   const number1 = await loadTexture(gl, './1.png');
   const number2 = await loadTexture(gl, './2.png');
@@ -164,12 +171,7 @@ const init = async () => {
   ];
 
   const pipes: { x: number; y: number }[] = [];
-  const trees: { x: number; y: number }[] = [
-    {
-      x: 0,
-      y: 80
-    }
-  ];
+
   const wires: { x: number; y: number }[] = [
     {
       x: 0,
@@ -184,6 +186,63 @@ const init = async () => {
       y: 65
     }
   ];
+  const clouds: { x: number; y: number }[] = [
+    {
+      x: 0,
+      y: 65
+    },
+    {
+      x: 25,
+      y: 65
+    },
+    {
+      x: 50,
+      y: 65
+    }
+  ];
+  const houses: { x: number; y: number }[] = [
+    {
+      x: 0,
+      y: 65
+    },
+    {
+      x: 25,
+      y: 65
+    },
+    {
+      x: 50,
+      y: 65
+    }
+  ];
+  const skys: { x: number; y: number }[] = [
+    {
+      x: 0,
+      y: 65
+    },
+    {
+      x: 25,
+      y: 65
+    },
+    {
+      x: 50,
+      y: 65
+    }
+  ];
+  const trees: { x: number; y: number }[] = [
+    {
+      x: 0,
+      y: 65
+    },
+    {
+      x: 25,
+      y: 65
+    },
+    {
+      x: 50,
+      y: 65
+    }
+  ];
+
   let treeIndex = 0;
   let wireIndex = 0;
   // const treebase = {
@@ -221,11 +280,15 @@ const init = async () => {
   let indexMole = 0;
   const DROP_RATE = 4;
   let accumulate = 0; //?
+  let accuSky = 0;
+  let accuHouse = 0;
+  let accuGround = 0;
   let accumulate1 = 0; //?
   let accuTree = 0;
   let accumulate2 = 0;
   let accuPipe = 0;
   let accumulateBee2 = 0;
+  let indexCloud;
 
   const mole_SIZE = 10;
 
@@ -238,6 +301,7 @@ const init = async () => {
   let stateTime = 0; //?
   let accumulateScore = 0;
   let accuUp = 0;
+  let accuCloud = 0;
   gl.clearColor(0, 0, 0, 1);
   createGameLoop(delta => {
     accuUp += delta;
@@ -246,7 +310,6 @@ const init = async () => {
     }
     if (inputHandler.isTouched() && stop == 0) {
       bird.y = bird.y - 1.5;
-      console.log(accuUp);
       Time_gia_toc = 0;
       rotateAngle = 7;
       roatateTime = 0;
@@ -263,12 +326,16 @@ const init = async () => {
       score = 0;
       stop = 0;
     }
+    accuCloud += delta;
     accumulate += delta;
     accumulate1 += delta;
     accumulate2 += delta;
     accuPipe += delta;
     accuTree += delta;
     accumulateBee2 += delta;
+    accuGround += delta;
+    accuHouse += delta;
+    accuSky += delta;
 
     if (accuPipe > 1.5 && stop == 0) {
       accuPipe = 0;
@@ -279,78 +346,128 @@ const init = async () => {
         y: Math.floor(Math.random() * (80 - 30 + 1)) + 30
       });
     }
-    // if (accumulate2 > 3 && stop == 0) {
-    //   accumulate2 = 0;
+    if (accumulate2 > 3 && stop == 0) {
+      accumulate2 = 0;
 
-    //   bikes1.push({
-    //     x: 25,
-    //     y: 80,
-    //     speedup: Math.random()
-    //   });
+      bikes1.push({
+        x: 25,
+        y: 80,
+        speedup: Math.random()
+      });
 
-    //   bikes2.push({
-    //     x: 15,
-    //     y: 80,
-    //     speedup: Math.random()
-    //   });
-    // }
+      bikes2.push({
+        x: 15,
+        y: 80,
+        speedup: Math.random()
+      });
+    }
 
-    // for (let bee of bees01) {
-    //   indexBee = bees01.indexOf(bee);
-    //   lastIndexBee01 = bees01.length - 1;
+    if (accumulate > 2 && stop == 0) {
+      accumulate = 0;
 
-    //   if (accumulate > 2 && stop == 0) {
-    //     accumulate = 0;
+      bees01.push({
+        x: 80,
+        y: Math.floor(Math.random() * (50 - 30 + 1)) + 30,
+        powerup: Math.random()
+      });
+    }
 
-    //     bees01.push({
-    //       x: 50,
-    //       y: Math.floor(Math.random() * (50 - 30 + 1)) + 30,
-    //       powerup: Math.random()
-    //     });
-    //   }
-    //   if (bee.x < -15) {
-    //     bees01.splice(indexBee, 1);
-    //   }
-    // }
-    // for (let bee of bees02) {
-    //   indexBee2 = bees02.indexOf(bee);
-    //   lastIndexBee02 = bees02.length - 1;
+    if (accumulateBee2 > 2 && stop == 0) {
+      accumulateBee2 = 0;
 
-    //   if (accumulateBee2 > 2 && stop == 0) {
-    //     accumulateBee2 = 0;
+      bees02.push({
+        x: 60,
+        y: Math.floor(Math.random() * (60 - 20 + 1)) + 20,
+        powerup: Math.random()
+      });
+    }
 
-    //     bees02.push({
-    //       x: 50,
-    //       y: Math.floor(Math.random() * (60 - 20 + 1)) + 20,
-    //       powerup: Math.random()
-    //     });
-    //   }
-    //   if (bee.x < -9) {
-    //     bees02.splice(indexBee2, 1);
-    //   }
-    // }
-    // for (let wire of wires) {
-    //   indexWire = wires.indexOf(wire);
-    //   lastIndexWire = wires.length - 1;
+    for (let sky of skys) {
+      indexSky = skys.indexOf(sky);
+      lastIndexSky = skys.length - 1;
 
-    //   if (accumulate1 > 4 && stop == 0) {
-    //     accumulate1 = 0;
+      if (accuSky > 4 && stop == 0) {
+        accuSky = 0;
 
-    //     wires.push({
-    //       x: wires[lastIndexWire].x + 25,
-    //       y: 65
-    //     });
-    //   }
-    //   if (wire.x < -25) {
-    //     wires.splice(indexWire, 1);
-    //   }
-    // }
+        skys.push({
+          x: skys[lastIndexSky].x + 25,
+          y: 65
+        });
+      }
+      if (sky.x < -25) {
+        skys.splice(indexSky, 1);
+      }
+    }
+    for (let house of houses) {
+      indexHouse = houses.indexOf(house);
+      lastIndexHouse = houses.length - 1;
+
+      if (accuHouse > 4 && stop == 0) {
+        accuHouse = 0;
+
+        houses.push({
+          x: houses[lastIndexHouse].x + 25,
+          y: 65
+        });
+      }
+      if (house.x < -25) {
+        houses.splice(indexHouse, 1);
+      }
+    }
+    for (let tree of trees) {
+      indexTree = trees.indexOf(tree);
+      lastIndexTree = trees.length - 1;
+
+      if (accuTree > 4 && stop == 0) {
+        accuTree = 0;
+
+        trees.push({
+          x: trees[lastIndexTree].x + 25,
+          y: 65
+        });
+      }
+      if (tree.x < -25) {
+        trees.splice(indexTree, 1);
+      }
+    }
+    for (let wire of wires) {
+      indexWire = wires.indexOf(wire);
+      lastIndexWire = wires.length - 1;
+
+      if (accumulate1 > 4 && stop == 0) {
+        accumulate1 = 0;
+
+        wires.push({
+          x: wires[lastIndexWire].x + 25,
+          y: 65
+        });
+      }
+      if (wire.x < -25) {
+        wires.splice(indexWire, 1);
+      }
+    }
+    for (let cloud of clouds) {
+      indexCloud = clouds.indexOf(cloud);
+      lastIndexCloud = clouds.length - 1;
+
+      if (accuCloud > 4 && stop == 0) {
+        accuCloud = 0;
+
+        clouds.push({
+          x: clouds[lastIndexCloud].x + 25,
+          y: 65
+        });
+      }
+      if (cloud.x < -25) {
+        clouds.splice(indexCloud, 1);
+      }
+    }
 
     for (let ground of grounds) {
       indexGround = grounds.indexOf(ground);
       lastIndexGround = grounds.length - 1;
-      if (accuTree > 0.3 && stop == 0) {
-        accuTree = 0;
+      if (accuGround > 0.3 && stop == 0) {
+        accuGround = 0;
 
         grounds.push({
           x: grounds[lastIndexGround].x + 10,
@@ -376,72 +493,120 @@ const init = async () => {
     if (Time_gia_toc >= 2) {
       Time_gia_toc = 0;
     }
-    // console.log(Time_gia_toc);
+    console.log(houses.length);
+    for (let sky of skys) {
+      batch.draw(skyImg, sky.x, sky.y - 70, 25, 75);
 
-    // for (let wire of wires) {
-    //   batch.draw(skyImg, wire.x, wire.y - 70, 25, 75);
+      // batch.draw(house, wire.x, wire.y, 25, 25);
 
-    //   batch.draw(cloud, wire.x, wire.y - 3, 25, 25);
+      // batch.draw(treeImg, wire.x, wire.y + 15, 25, 10);
 
-    //   batch.draw(house, wire.x, wire.y, 25, 25);
+      if (stop == 0) {
+        sky.x = sky.x - 0.1;
+      }
+    }
+    for (let cloud of clouds) {
+      batch.draw(cloudImg, cloud.x, cloud.y - 3, 25, 25);
 
-    //   batch.draw(wireImg, wire.x, wire.y, 25, 25);
+      // batch.draw(house, wire.x, wire.y, 25, 25);
 
-    //   batch.draw(treeImg, wire.x, wire.y + 15, 25, 10);
+      // batch.draw(treeImg, wire.x, wire.y + 15, 25, 10);
 
-    //   if (stop == 0) {
-    //     wire.x = wire.x - 0.1;
-    //   }
-    // }
+      if (stop == 0) {
+        cloud.x = cloud.x - 0.1;
+      }
+    }
+    for (let house of houses) {
+      // batch.draw(cloudImg, house.x, house.y - 3, 25, 25);
 
-    // for (let bee of bees01) {
-    //   batch.draw(bee1Img, bee.x, bee.y, 15, 8.55);
+      batch.draw(houseImg, house.x, house.y, 25, 25);
 
-    //   if (bee.powerup < 0.4 && stop == 0) {
-    //     bee.x = bee.x - 0.2;
-    //   } else {
-    //     if (stop == 0) {
-    //       bee.x = bee.x - 0.5;
-    //     }
-    //   }
-    // }
+      // batch.draw(treeImg, wire.x, wire.y + 15, 25, 10);
 
-    // for (let bee of bees02) {
-    //   batch.draw(bee2Img, bee.x, bee.y, 9, 7.7);
+      if (stop == 0) {
+        house.x = house.x - 0.1;
+      }
+    }
+    for (let wire of wires) {
+      // batch.draw(cloudImg, house.x, house.y - 3, 25, 25);
 
-    //   if (bee.powerup < 0.2 && stop == 0) {
-    //     bee.x = bee.x - 0.2;
-    //   } else {
-    //     if (stop == 0) {
-    //       bee.x = bee.x - 0.8;
-    //     }
-    //   }
-    // }
+      batch.draw(wireImg, wire.x, wire.y, 25, 25);
 
-    // for (let bike of bikes1) {
-    //   indexBike = bikes1.indexOf(bike);
+      // batch.draw(treeImg, wire.x, wire.y + 15, 25, 10);
 
-    //   batch.draw(bikeImg, bike.x, bike.y, 60, 10);
+      if (stop == 0) {
+        wire.x = wire.x - 0.1;
+      }
+    }
 
-    //   if (stop == 0) {
-    //     bike.x = bike.x - 0.3;
-    //   }
-    //   if (bike.x < -200) {
-    //     bikes1.splice(indexBike, 1);
-    //   }
-    // }
-    // for (let bike of bikes2) {
-    //   indexBike2 = bikes2.indexOf(bike);
+    for (let tree of trees) {
+      batch.draw(treeImg, tree.x, tree.y + 15, 25, 15);
 
-    //   batch.draw(bike2Img, bike.x, bike.y, 60, 10);
+      // batch.draw(house, wire.x, wire.y, 25, 25);
 
-    //   if (stop == 0) {
-    //     bike.x = bike.x - 0.5;
-    //   }
-    //   if (bike.x < -200) {
-    //     bikes2.splice(indexBike2, 1);
-    //   }
-    // }
+      // batch.draw(treeImg, wire.x, wire.y + 15, 25, 10);
+
+      if (stop == 0) {
+        tree.x = tree.x - 0.1;
+      }
+    }
+
+    for (let bee of bees01) {
+      indexBee = bees01.indexOf(bee);
+      batch.draw(bee1Img, bee.x, bee.y, 15, 8.55);
+
+      if (bee.powerup < 0.4 && stop == 0) {
+        bee.x = bee.x - 0.2;
+      } else {
+        if (stop == 0) {
+          bee.x = bee.x - 0.5;
+        }
+      }
+      if (bee.x < -300) {
+        bees01.splice(indexBee, 1);
+      }
+    }
+
+    for (let bee of bees02) {
+      indexBee2 = bees02.indexOf(bee);
+      batch.draw(bee2Img, bee.x, bee.y, 9, 7.7);
+
+      if (bee.powerup < 0.2 && stop == 0) {
+        bee.x = bee.x - 0.2;
+      } else {
+        if (stop == 0) {
+          bee.x = bee.x - 0.8;
+        }
+      }
+      if (bee.x < -350) {
+        bees02.splice(indexBee2, 1);
+      }
+    }
+
+    for (let bike of bikes1) {
+      indexBike = bikes1.indexOf(bike);
+
+      batch.draw(bikeImg, bike.x, bike.y, 60, 10);
+
+      if (stop == 0) {
+        bike.x = bike.x - 0.3;
+      }
+      if (bike.x < -200) {
+        bikes1.splice(indexBike, 1);
+      }
+    }
+    for (let bike of bikes2) {
+      indexBike2 = bikes2.indexOf(bike);
+
+      batch.draw(bike2Img, bike.x, bike.y, 60, 10);
+
+      if (stop == 0) {
+        bike.x = bike.x - 0.5;
+      }
+      if (bike.x < -200) {
+        bikes2.splice(indexBike2, 1);
+      }
+    }
     accumulateScore += delta;
     for (let pipe of pipes) {
       batch.draw(pipeImg, pipe.x, pipe.y, 10, 100);
